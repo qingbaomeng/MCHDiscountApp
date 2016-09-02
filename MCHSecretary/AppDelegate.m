7255be5d786a37c4cf1f38b20d5876132edd78e0
qingbaomeng <439020255@qq.com>
2016-09-02 17:54:54 +0800

    appdelage

--- MCHSecretary/AppDelegate.m
+++ MCHSecretary/AppDelegate.m
@@ -96,6 +96,9 @@
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
+
+
+
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
